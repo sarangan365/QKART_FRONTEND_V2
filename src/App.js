f6fafdc858d832a64e7248ebf99dc2103d9f5b70
Sarangan S <sarangan365@gmail.com>
2022-04-22 04:10:35 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Checkout from "./components/Checkout";
 import Thanks from "./components/Thanks";
 
 export const config = {
-  endpoint: `https://43.204.81.73:8081/api/v1`,
+  endpoint: `${ipConfig.workspaceIp}:8082/api/v1`,
 };
 
 function App() {
